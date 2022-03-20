2240f5277f41c4b57ace17dc577438fa9dc5a983
unknown <mrafiul.alam7@gmail.com>
2022-03-20 23:59:42 +0600

    first-commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+const ObjectId = require('mongodb').ObjectId
+app.use(cors())
+app.use(express.json())
+
+require ('dotenv').config()
+
+// hospital-web
+// ZNzgXH6QLBPTGbKu
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlklf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const run = async() =>{
+      try{
+            await client.connect()
+            console.log("database connected")
+
+            
+            
+
+            app.get('/', async(req,res) =>{
+                  console.log("server running")
+                  res.send('Server Running')
+            })
+            app.listen(port,() =>{
+                  console.log("listening from port",port)
+            })
+
+      }
+      finally{
+                  // await client.close();
+      }
+
+}
+run().catch(console.dir);
